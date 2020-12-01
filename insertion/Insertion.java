fdb1bc144302c6c287c9396bc7bdd5fde375fada
Denis <dineresc@gmail.com>
2020-12-01 10:26:03 -0300

    add counter time

--- insertion/Insertion.java
+++ insertion/Insertion.java
@@ -3,7 +3,7 @@ public class Insertion {
         int comparacoes = 0;
         int trocas = 0;
         int aux = 0;
-        int[] numbers = {3,1,4,2,6,8,5};
+        int[] numbers = {15,76,78,28,47,71,69,37,81,10,66,91,32,64,34,41,54,51,50,18,3,29,5,2,17,59,82,77,62,100,74,57,11,7,21,70,80,68,35};
         
         for (int k = 1; k < numbers.length; k++) {
             for (int i = k; i > 0; i--) {
--- insertion/insertion_js.js
+++ insertion/insertion_js.js
@@ -1,24 +1,27 @@
-var cartas = [3,4,1,2,7,5]
+const { performance } = require('perf_hooks');
+
+var cartas = [15,76,78,28,47,71,69,37,81,10,66,91,32,64,34,41,54,51,50,18,3,29,5,2,17,59,82,77,62,100,74,57,11,7,21,70,80,68,35];
 let comparacoes = 0;
 let trocas = 0;
 
-for (let k = 1; k < cartas.length; k++) {
-    for (let i = k-1; i >= 0 ; i--) {
-        comparacoes++;
-        if (cartas[i] < cartas[i-1]) {
-            console.log(cartas[i] + " <> " + cartas[i-1]);
-            let aux = cartas[i-1];
-            cartas[i-1] = cartas[i];
-            cartas[i] = aux;
-            
-            trocas+=1;
-        } else {
-            break;
-        }
-    }
-}
+const start = performance.now();
+insertion(cartas);
+const end = performance.now();
+console.log(`time: ${end-start} miliseconds`); 
 
-console.log("array ordenado: ");
-console.log(cartas);
-console.log("comparacoes: " + comparacoes);
-console.log("trocas: " + trocas);
+function insertion(cartas) {
+  for (let k = 1; k < cartas.length; k++) {
+    for (let i = k - 1; i >= 0; i--) {
+      comparacoes++;
+      if (cartas[i] < cartas[i - 1]) {
+        let aux = cartas[i - 1];
+        cartas[i - 1] = cartas[i];
+        cartas[i] = aux;
+
+        trocas += 1;
+      } else {
+        break;
+      }
+    }
+  }
+}
