7e32b09ef3281aa7f9e5f335e5df551ed40f140f
Denis <dineresc@gmail.com>
2020-12-02 19:31:10 -0300

    isertion

--- insertion/Insertion.java
+++ insertion/Insertion.java
@@ -3,16 +3,23 @@ public class Insertion {
         int comparacoes = 0;
         int trocas = 0;
         int aux = 0;
-        int[] numbers = {15,76,78,28,47,71,69,37,81,10,66,91,32,64,34,41,54,51,50,18,3,29,5,2,17,59,82,77,62,100,74,57,11,7,21,70,80,68,35};
-        
+        int[] numbers = {50,49,48,47,46,45,44,43,42,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
+        var insertion = new Insertion();
+
+        long startTime = System.nanoTime();
+        insertion.sort(numbers);
+        long elapsedTime = System.nanoTime() - startTime;
+        System.out.println(elapsedTime);
+    }
+
+    public void sort(int[] numbers) {
+        int aux = 0;
         for (int k = 1; k < numbers.length; k++) {
             for (int i = k; i > 0; i--) {
-                comparacoes++;
                 if (numbers[i] < numbers[i-1]) {
                     aux = numbers[i-1];
                     numbers[i-1] = numbers[i];
                     numbers[i] = aux;
-                    trocas++;
                 }    
             }
         }
--- insertion/insertion_js.js
+++ insertion/insertion_js.js
@@ -1,24 +1,23 @@
-const { performance } = require('perf_hooks');
+const { performance } = require("perf_hooks");
 
-var cartas = [15,76,78,28,47,71,69,37,81,10,66,91,32,64,34,41,54,51,50,18,3,29,5,2,17,59,82,77,62,100,74,57,11,7,21,70,80,68,35];
+var cartas = [
+    50,49,48,47,46,45,44,43,42,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1
+];
 let comparacoes = 0;
 let trocas = 0;
 
 const start = performance.now();
 insertion(cartas);
 const end = performance.now();
-console.log(`time: ${end-start} miliseconds`); 
+console.log(`time: ${end - start} miliseconds`);
 
 function insertion(cartas) {
   for (let k = 1; k < cartas.length; k++) {
     for (let i = k - 1; i >= 0; i--) {
-      comparacoes++;
       if (cartas[i] < cartas[i - 1]) {
         let aux = cartas[i - 1];
         cartas[i - 1] = cartas[i];
         cartas[i] = aux;
-
-        trocas += 1;
       } else {
         break;
       }
